35cdaef4cef659bff9307688b110b8b2b836a0d4
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-07-03 11:04:41 +0500

    Refactoring code

---
+++ FakeNFT/App/AppDelegate.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
