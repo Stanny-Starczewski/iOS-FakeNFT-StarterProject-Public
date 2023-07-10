43679335be6f1600b9dd875b3299e8ac46054029
Stanley2023 <stanislav.starczewski@gmail.com>
2023-07-10 05:04:22 -0400

    Icons is added

---
+++ FakeNFT/DesignSystem/Icons.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIImage {
+    enum Icons {
+        static let backward = UIImage(named: "Backward")
+        static let edit = UIImage(named: "Edit")
+        static let filter = UIImage(named: "Filter")
+        static let filledTrash = UIImage(named: "FilledTrash")
+        static let emptyTrash = UIImage(named: "EmptyTrash")
+        static let activeLike = UIImage(named: "ActiveLike")
+        static let inactiveLike = UIImage(named: "InactiveLike")
+        static let emptyStar = UIImage(named: "EmptyStar")
+        static let filledStar = UIImage(named: "FilledStar")
+    }
+}
