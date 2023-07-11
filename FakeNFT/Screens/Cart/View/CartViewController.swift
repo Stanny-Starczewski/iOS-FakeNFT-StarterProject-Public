9a6bcd894b8d559342a044a756340a5de331f5af
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-07-11 14:34:55 +0500

    Adds cart view controller

--- FakeNFT/Screens/Cart/View/CartViewController.swift
+++ FakeNFT/Screens/Cart/View/CartViewController.swift
@@ -1,11 +1,24 @@
 import UIKit
 
+protocol CartViewProtocol: AnyObject {
+    func showViewController(_ vc: UIViewController)
+}
+
 final class CartViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let bottomViewCornerRadius: CGFloat = 12
+        static let iconSort = UIImage(named: "icon-sort")
+        static let paymentButtonText = "К оплате"
+    }
     
     // MARK: - UI
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .appWhite
         tableView.separatorStyle = .none
         tableView.contentInset = UIEdgeInsets(
             top: 20,
@@ -19,6 +32,62 @@ final class CartViewController: UIViewController {
         )
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
+    }()
+    
+    private lazy var bottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .appLightGrey
+        view.layer.cornerRadius = Constants.bottomViewCornerRadius
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var quantityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3 NFT"
+        label.font = .regular15
+        label.textColor = .appBlack
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var amountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "5,34 ETH"
+        label.font = .bold17
+        label.textColor = .customGreen
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var totalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [quantityLabel, amountLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var paymentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .appBlack
+        button.tintColor = .customWhite
+        button.layer.cornerRadius = 16
+        button.setTitle(Constants.paymentButtonText, for: .normal)
+        button.titleLabel?.font = .bold17
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var bottomStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [totalStackView, paymentButton])
+        stackView.axis = .horizontal
+        stackView.spacing = 24
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     private let presenter: CartPresenterProtocol
@@ -36,7 +105,6 @@ final class CartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupView()
         setConstraints()
         setupNavigationController()
@@ -48,11 +116,13 @@ final class CartViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .appWhite
         view.addSubview(tableView)
+        view.addSubview(bottomView)
+        bottomView.addSubview(bottomStackView)
     }
     
     private func setupNavigationController() {
         let sortButton = UIBarButtonItem(
-            image: UIImage(named: "icon-sort"),
+            image: Constants.iconSort,
             style: .plain,
             target: self,
             action: #selector(sortButtonTapped)
@@ -112,7 +182,17 @@ extension CartViewController {
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+//            bottomView.heightAnchor.constraint(equalToConstant: 76),
+            
+            bottomStackView.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 16),
+            bottomStackView.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16),
+            bottomStackView.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16),
+            bottomStackView.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -16)
         ])
     }
 }
