03e801ded321d54dfe395700e63bf4d34af909ef
Stanley2023 <stanislav.starczewski@gmail.com>
2023-07-21 01:59:28 -0400

    Fixed RatingStackView

--- FakeNFT/Screens/Catalog/View/Collection/RatingStackView.swift
+++ FakeNFT/Screens/Catalog/View/Collection/RatingStackView.swift
@@ -1,38 +1,31 @@
 import UIKit
 
 final class RatingStackView: UIStackView {
-    let countOfStars: Int
+    private let fillStarImage: UIImage? = UIImage(named: "FilledStar")
+    private let emptyStarImage: UIImage? = UIImage(named: "EmptyStar")
     
     init(countOfStars: Int) {
-        self.countOfStars = countOfStars
         super.init(frame: .zero)
+        axis = .horizontal
+        spacing = 2
         
-        setupView()
+        for starTag in 1...countOfStars {
+            let imageView = UIImageView()
+            imageView.image = emptyStarImage
+            imageView.tag = starTag
+            addArrangedSubview(imageView)
+        }
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupView() {
-        axis = .horizontal
-        spacing = 2
-        
-        for _ in 0..<countOfStars {
-            let starImageView = UIImageView(image: UIImage(systemName: "star.fill"))
-            starImageView.tintColor = .lightGray
-            
-            NSLayoutConstraint.activate([
-                starImageView.widthAnchor.constraint(equalToConstant: 12),
-                starImageView.heightAnchor.constraint(equalToConstant: 12)
-            ])
-            addArrangedSubview(starImageView)
-        }
-    }
-    
     func setupRating(rating: Int) {
-        for star in 0..<rating {
-            arrangedSubviews[star].tintColor = .yellow
+        for subview in subviews {
+            if let imageView = subview as? UIImageView {
+                imageView.image = imageView.tag > rating ? emptyStarImage : fillStarImage
+            }
         }
     }
 }
