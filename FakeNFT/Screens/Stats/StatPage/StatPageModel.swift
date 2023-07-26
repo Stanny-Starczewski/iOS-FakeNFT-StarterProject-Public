7cb7a48b4e028380f136191e666fbacc1dc15adb
Alexander Farizanov <farizanov@gmail.com>
2023-07-26 16:39:42 +0300

    Add PRIVATE for some constants

--- FakeNFT/Screens/Stats/StatPage/StatPageModel.swift
+++ FakeNFT/Screens/Stats/StatPage/StatPageModel.swift
@@ -1,12 +1,11 @@
 import Foundation
 
 final class StatPageModel {
-   let networkClient = DefaultNetworkClient()
+   private let networkClient = DefaultNetworkClient()
 
     func getUsers(completion: @escaping (Result<[User], Error>) -> Void) {
 
         let request = Request(endpoint: URL(string: Config.baseUrl + "/users"), httpMethod: .get)
-        
         networkClient.send(request: request, type: [User].self, onResponse: completion)
     }
 }
